a700b1cef84fc6591196a02f089bac9244845cfa
lturbang <lturbang@student.s19.be>
2021-01-27 15:48:47 +0100

    [philo_one] clean + add utils + implement print status function

--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_one.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 15:27:09 by user42            #+#    #+#             */
+/*   Updated: 2021/01/27 15:47:20 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_ONE_H
 # define PHILO_ONE_H
 
@@ -12,5 +24,21 @@
 # define THINK 2
 # define FORK 3
 # define DEAD 4
+
+/*
+**		UTILS
+*/
+
+unsigned int	ft_strlen(const char *s);
+char			*ft_strdup(const char *src);
+void			ft_putnbr_ull(unsigned long long n);
+void			ft_putstr(char *str);
+void			ft_putnbr(int n);
+
+/*
+**		PHILO
+*/
+
+void			print_status(unsigned long long ms, int philo_id, int status);
 
 #endif
--- philo_one/philo_one_utils.c
+++ philo_one/philo_one_utils.c
@@ -1,16 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_one_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 15:26:54 by user42            #+#    #+#             */
+/*   Updated: 2021/01/27 15:47:15 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo_one.h"
 
-void			ft_putnbr_ull(unsigned long long n)
+void			print_status(unsigned long long ms, int philo_id, int status)
 {
-	unsigned long long		div;
-	char					mod;
-
-	div = n / 10;
-	mod = n % 10;
-	if (div)
-		ft_putnbr_ull(div);
-	mod += 48;
-	write(1, &mod, 1);
+	ft_putnbr_ull(ms);
+	write(1, "ms ", 3);
+	ft_putnbr(philo_id);
+	if (status == SLEEP)
+		ft_putstr(" is sleeping\n");
+	else if (status == EAT)
+		ft_putstr(" is eating\n");
+	else if (status == FORK)
+		ft_putstr(" has taken a fork\n");
+	else if (status == THINK)
+		ft_putstr(" is thinking\n");
+	else if (status == DEAD)
+		ft_putstr(" died\n");
 }
-
-
