b0644f29c83a63af168f3e3a18f997dda37690eb
Loic Turbang <lturbang@shi-r1-p1.s19.be>
2021-01-27 15:06:41 +0100

    [philo_one] start files + Makefile

---
+++ philo_one/philo_one.c
@@ -0,0 +1,7 @@
+#include "philo_one.h"
+
+int		main(void)
+{
+	printf("Ok c'est good\n");
+	return (0);
+}
---
+++ philo_one/philo_one.h
@@ -0,0 +1,10 @@
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+
+# include <stdlib.h>
+# include <stdio.h> //remove
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
