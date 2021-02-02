619dc13f799fea1d95be53970f98cbe86af92c8b
lturbang <lturbang@student.s19.be>
2021-02-02 09:31:23 +0100

    [philo_one] debug death 2 60 60 60

--- philo_one/srcs/philo_one.c
+++ philo_one/srcs/philo_one.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 15:27:00 by user42            #+#    #+#             */
-/*   Updated: 2021/02/02 09:27:59 by lturbang         ###   ########.fr       */
+/*   Updated: 2021/02/02 09:30:42 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ void	*init_check_death(void *arg)
 		i = -1;
 		while (p->philos[++i] && i < p->nb_philos)
 		{
+			printf("starve_t_delta %lu\n", get_delta_time(p->philos[i]) - p->philos[i]->last_eat);
 			if (p->philos[i]->last_eat != 0 && (get_delta_time(p->philos[i]) - p->philos[i]->last_eat) >= (unsigned long)p->tt_die)
 			{
 				printf("starve_t_delta %lu tt_die %d\n", get_delta_time(p->philos[i]) - p->philos[i]->last_eat, p->tt_die);
