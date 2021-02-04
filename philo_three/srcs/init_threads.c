e844c16ebf523a56558eff2b6c6f1f83d0c7ed2c
Loic Turbang <t.loic@hotmail.com>
2021-02-04 09:59:05 +0100

    [philo_three] init thread only if must_eat != -1

--- philo_three/srcs/init_threads.c
+++ philo_three/srcs/init_threads.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 13:23:09 by user42            #+#    #+#             */
-/*   Updated: 2021/02/04 09:39:29 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/04 09:43:28 by lturbang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,8 +122,9 @@ int		create_threads(t_p *p)
 	}
 	if (pthread_create(&p->th_death, NULL, &check_death, p) != 0)
 		return (-1);
-	if (pthread_create(&p->th_must_eat, NULL, &update_must_eat, p) != 0)
-		return (-1);
+	if (p->must_eat_nb != -1)
+		if (pthread_create(&p->th_must_eat, NULL, &update_must_eat, p) != 0)
+			return (-1);
 	i = -1;
 	while (++i < p->nb_philos)
 	{
