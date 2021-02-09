283234a16301cf4d1e041517994657a2d3c517fd
Loic Turbang <t.loic@hotmail.com>
2021-02-09 09:34:43 +0100

    [philo_three] fix must_eat_nb

--- philo_three/srcs/philo.c
+++ philo_three/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 12:33:49 by user42            #+#    #+#             */
-/*   Updated: 2021/02/08 17:37:16 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/09 09:32:57 by lturbang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,11 @@ void	philo_life(t_p *p, t_philo *philo)
 		print_status(get_delta_time(), philo->id, FORK, p);
 		print_status(get_delta_time(), philo->id, FORK, p);
 		print_status(get_delta_time(), philo->id, EAT, p);
+		if (philo->nb_eat >= p->must_eat_nb - 1 && p->must_eat_nb != -1)
+			sem_post(philo->must_eat);
+		philo->nb_eat++;
 		philo->last_eat = get_delta_time();
 		wait_ms(p->tt_eat);
-		philo->nb_eat++;
-		if (philo->nb_eat >= p->must_eat_nb)
-			sem_post(philo->must_eat);
 		sem_post(p->forks);
 		sem_post(p->forks);
 		print_status(get_delta_time(), philo->id, SLEEP, p);
