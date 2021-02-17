4760287a55deb342f2cb45bc57923b23c953d8ef
lturbang <lturbang@student.s19.be>
2021-02-17 17:14:32 +0100

    [philo_two] norme done

--- philo_two/srcs/monitoring.c
+++ philo_two/srcs/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 12:33:46 by user42            #+#    #+#             */
-/*   Updated: 2021/02/17 16:56:53 by lturbang         ###   ########.fr       */
+/*   Updated: 2021/02/17 17:13:14 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,14 @@ void	*print_quit(t_p *p, t_philo *philo)
 	sem_wait(p->print);
 	write(1, str, ft_strlen(str));
 	free(str);
+	free_print_list(p);
+	sem_post(p->sem_dead);
+	return (NULL);
+}
+
+void	*print_eat_quit(t_p *p)
+{
+	print_lst(p);
 	free_print_list(p);
 	sem_post(p->sem_dead);
 	return (NULL);
@@ -48,12 +56,7 @@ void	*monitoring(void *arg)
 			usleep(19);
 		}
 		if (p->finish_eat >= p->must_eat_nb && p->must_eat_nb != -1)
-		{
-			print_lst(p);
-			free_print_list(p);
-			sem_post(p->sem_dead);
-			return (NULL);
-		}
+			return (print_eat_quit(p));
 	}
 	return (NULL);
 }
